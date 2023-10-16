deb1d12349de408b0ca27ca3d8187e3ee6de0748
MD TANVIRUL ISLAM <66683461+tanvir35web@users.noreply.github.com>
2023-10-17 01:03:43 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 function startTimer(){
 
-
-
-let day = document.getElementById("day");
-let hr = document.getElementById("hr");
-let min = document.getElementById("min");
-let sec = document.getElementById("sec");
-let localTime = document.getElementById("input-date").value;
-
-let milliSecond = Date.parse(localTime);
-
-console.log(milliSecond);
-
-
-var x = setInterval(function(){
-    var now = new Date().getTime();
-    var distance = milliSecond - now;
-
-    var days = Math.floor( distance / (1000 * 60 * 60 * 24) );
-    var hours = Math.floor( (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) );
-    var minutes = Math.floor( (distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor( (distance % (1000 * 60) ) / 1000);
-
-    day.innerHTML = (days  < 10 ? "0" : "") + days;
-    hr.innerHTML = (hours  < 10 ? "0" : "") + hours;
-    min.innerHTML = (minutes  < 10 ? "0" : "") + minutes;
-    sec.innerHTML = (seconds  < 10 ? "0" : "") + seconds;
-
-}, 1000); 
+    let day = document.getElementById("day");
+    let hr = document.getElementById("hr");
+    let min = document.getElementById("min");
+    let sec = document.getElementById("sec");
+    let localTime = document.getElementById("input-date").value;
+    
+    let milliSecond = Date.parse(localTime);
+    
+    console.log(milliSecond);
+    
+    
+    var x = setInterval(function(){
+        var now = new Date().getTime();
+        var distance = milliSecond - now;
+    
+        var days = Math.floor( distance / (1000 * 60 * 60 * 24) );
+        var hours = Math.floor( (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) );
+        var minutes = Math.floor( (distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor( (distance % (1000 * 60) ) / 1000);
+    
+        day.innerHTML = (days  < 10 ? "0" : "") + days;
+        hr.innerHTML = (hours  < 10 ? "0" : "") + hours;
+        min.innerHTML = (minutes  < 10 ? "0" : "") + minutes;
+        sec.innerHTML = (seconds  < 10 ? "0" : "") + seconds;
+    
+    }, 1000); 
 
 }
 
